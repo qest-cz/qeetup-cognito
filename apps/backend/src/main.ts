690b7d33d9ecf5dc0f4b9083158e31fc9580f420
Cyril Urban <cyril.urban@ack.ee>
2022-08-28 20:38:51 +0200

    feat(be): add proof of concept for cognito

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import { verifyToken } from './app/auth';
 
 const app = express();
 
@@ -6,7 +7,6 @@ type Ticket = {
   title: string;
   id: number;
 };
-
 
 const tickets: Ticket[] = [
   {
@@ -19,12 +19,26 @@ const tickets: Ticket[] = [
   }
 ];
 
-app.get('/api/tickets', (req, res) => {
+const verifyUser = (req, res) => {
+  const authorization = req?.headers.authorization
+  if (!authorization) {
+    throw new Error('Unauthorized 401')
+  }
+
+  const jwk = ['TODO from cognito'] as any [] // TODO
+  verifyToken(authorization, jwk)
+}
+
+const ticketController = (req, res) => {
   res.send(tickets);
-});
+}
+
+app.get('/api/tickets', /*verifyUser,*/ ticketController);
 
 const port = process.env.port || 3333;
+
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
+
 server.on('error', console.error);

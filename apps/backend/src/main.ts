d77ffd644b7642d39baf33648dcd22cf2ba95fc3
Cyril Urban <cyril.urban@ack.ee>
2022-08-28 19:11:30 +0200

    ref(nx): minnor refactor - prepare nx monoropo

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,30 @@
+import * as express from 'express';
+
+const app = express();
+
+type Ticket = {
+  title: string;
+  id: number;
+};
+
+
+const tickets: Ticket[] = [
+  {
+    title: `Install updates`,
+    id: 1
+  },
+  {
+    title: `Restore the backup`,
+    id: 2
+  }
+];
+
+app.get('/api/tickets', (req, res) => {
+  res.send(tickets);
+});
+
+const port = process.env.port || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
--- apps/frontend/jest.config.ts
+++ apps/frontend/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'qeetup-cognito',
+  displayName: 'frontend',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/qeetup-cognito',
+  coverageDirectory: '../../coverage/apps/frontend',
 };
